889caa58780e7fddc62d7b06bae5dd61dc001ee8
duskmage <Duskmage2009@gmail.com>
2025-09-18 14:41:11 +0300

    "Start the task"

---
+++ src/test/java/com/autoservice/feedbackbot/FeedbackBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoservice.feedbackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
